21366b6d1f503583be252ebdaf06c354bb48b9dc
Gabu <gabydavilaneves@gmail.com>
2022-06-25 19:00:41 -0300

    Entendendo melhor sobre const

---
+++ JS/aula7/index.js
@@ -0,0 +1,13 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não pode modificarovalor de uma constante
+// NÃO UTILIZE VAR,UTILIZE CONST.
+// Typeof = Saber o tipo de dado, se é number ou string
+const primeiroNumero = 5;
+const segundoNumero = 10; 
+const conta = primeiroNumero * segundoNumero 
+console.log(primeiroNumero + segundoNumero)
