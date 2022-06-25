d2ef190a6ebb5f56241949f91334c1ec9bc1c9ac
Gabu <gabydavilaneves@gmail.com>
2022-06-25 20:02:05 -0300

    calculando imc

---
+++ JS/aula8/index.js
@@ -0,0 +1,17 @@
+const nome = 'Luiz Otávio ';
+const sobrenome = 'Miranda';
+const idade = 18;
+const peso = 67;
+const alturaEmM = 1.70;
+
+let imc; //peso / (altura * altura)
+let anoNascimento;
+
+imc = peso / (alturaEmM * alturaEmM)
+anoNascimento = 2022 - 18
+
+console.log(imc, anoNascimento)
+
+const resposta = `${nome}${sobrenome} tem ${idade} anos e nasceu em ${anoNascimento} tem ${alturaEmM}m de altura, pesa ${peso}kg e seu imc é de ${imc}`
+
+console.log(resposta)
