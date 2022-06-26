526327852277aa8fb5bc042f2d2ba144285ba318
Gabu <gabydavilaneves@gmail.com>
2022-06-26 14:42:56 -0300

    feat: Aula 10: Operadores aritméticos, de atribuição e incremento
    Operadores aritméticos, de atribuição e incremento

---
+++ JS/aula10/index.js
@@ -0,0 +1,13 @@
+// Aritimeticos = + - / * || ** -> potencia || % -> resto da divisao
+
+// const num1 = 5;
+// const num2 = 2;
+// const num3 = 10;
+
+// console.log(num1 * num2 / num3)
+
+let contador = 0;
+contador += 2
+contador += 2
+contador += 2
+console.log(contador)
