3893ccc23314ba66dcd129360478caff9895be0d
Gabu <gabydavilaneves@gmail.com>
2022-06-26 16:06:18 -0300

    .

--- JS/aula10/index.js
+++ JS/aula10/index.js
@@ -6,8 +6,8 @@
 
 // console.log(num1 * num2 / num3)
 
-let contador = 0;
-contador += 2
-contador += 2
-contador += 2
+let contador = 2;
+contador *= 2
+contador *= 2
+contador **= 2
 console.log(contador)
