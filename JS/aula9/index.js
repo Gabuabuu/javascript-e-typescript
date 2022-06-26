5be2d43465e6f3ed4737958585586ca49c4159b1
Gabu <gabydavilaneves@gmail.com>
2022-06-25 21:45:56 -0300

    chore:

---
+++ JS/aula9/index.js
@@ -0,0 +1,21 @@
+// Tipos de dados primitivos -> String, Number, Undefined, null, boolean
+const nome = 'Luiz';// string
+const nomel = "Luiz";// string
+const nome2 = `Luiz`;// string
+const num1 = 10;// number
+const num2 = 10.52;// number
+let nomeAluno; // undefined -> não aponta pra local nenhuma na memória
+let sobrenomeAluno = null // Nulo -> não aponta pra local na memoria
+const aprovado = false; // Boolean = true or false (lógico)
+
+// console.log(typeof (sobrenomeAluno))
+
+let a = 1
+let b = 2
+
+console.log(a, b)
+
+a = 3
+
+console.log(a, b)
+
