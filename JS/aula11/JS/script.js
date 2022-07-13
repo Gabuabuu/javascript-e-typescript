343876f89dbc023f19a9317d5ce20bc7bec28082
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-07-13 15:06:12 -0300

    conclusão da aula 11:  Exercício de cálculo

---
+++ JS/aula11/JS/script.js
@@ -0,0 +1,9 @@
+const numero = prompt("Digite algum número");
+const numero2 = prompt("Digite outro número");
+
+const conversor = Number(numero);
+const conversor2 = Number(numero2)
+
+const conta = conversor + conversor2;
+
+alert(`A soma entre ${numero} e ${numero2} é igual a ${conta}`);
