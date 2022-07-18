a820fb680f74a76962854d700e7654c75e101e1c
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-07-18 19:00:39 -0300

    início do exercício da aula 13

--- JS/aula13/index.js
+++ JS/aula13/index.js
@@ -1,3 +1,9 @@
-let string = "Um \\texto"
+//            01234567
+let string = "Tres pratos de trigo para tres tigres tristes"
 
-console.log(string)
+console.log(string.toUpperCase())
+
+//console.log(string.slice(2, 6))
+
+
+//.indexOf (Serve para em qual indice começa tal texto)
