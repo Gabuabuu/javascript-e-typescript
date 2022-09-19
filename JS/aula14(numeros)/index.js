d60aaa4cae6cb50141bb9ea4c28a365c7392efb5
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-09-19 17:51:39 -0300

    início aula 15(objeto math)

---
+++ JS/aula14(numeros)/index.js
@@ -0,0 +1,23 @@
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 = ((num1 * 100) + (num2 * 100)) / 100 ;
+num1 = ((num1 * 100) + (num2 * 100)) / 100 ;
+num1 = ((num1 * 100) + (num2 * 100)) / 100 ;
+
+
+
+num1 = parseFloat(num1.toFixed(2))
+
+console.log((num1))
+console.log(Number.isInteger(num1))
+
+//console.log(num1.toString(2) + num2 );
+//console.log(typeof num1)
+//toString() converte número para string
+//console.log(num1.toFixed(2))
+//console.log(Number.isInteger(num1))
+//Number.isInteger(num1) verifica se é um número inteiro
+//let temp = num1 * "ola"
+//console.log(Number.isNaN(temp))
+//Number.isNaN(temp) diz quando nao é numero == NaN = true
