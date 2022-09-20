6244331527147875392597f89065f83e3de75bc8
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-09-19 22:17:30 -0300

    início aula 17

--- JS/aula16(funcoes)/index.js
+++ JS/aula16(funcoes)/index.js
@@ -1,5 +1,11 @@
-function saudacao(nome) {
-  
-}
+// function soma(x = 1, y = 1) {
+// const resultado = x + y;
+// return resultado;
+// }
 
-saudacao()
+// const resultado = soma(9, 9)
+// console.log(resultado )
+const raiz = n =>  n ** 0.5
+
+
+console.log(raiz(9))
