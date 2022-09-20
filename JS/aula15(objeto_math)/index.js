edd44864ae07d45095c19f0fcf29c95fa3f84022
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-09-19 21:01:09 -0300

    fim dos exercícios

---
+++ JS/aula15(objeto_math)/index.js
@@ -0,0 +1,12 @@
+let num1 = 9;
+console.log(num1 ** (1/2))//Raiz quadrada
+
+//let num2 = Math.floor(num1);Math.floor arredonda para baixo
+//let num2 = Math.ceil(num1);//Math.ceil arredonda para cima
+//let num2 = Math.round(num1);//Arredonda para cima ou para baixo(dependendo se ta abaixo ou acima de x.50)
+//console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6))//Pega o número maior
+//console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6)) //Pega o número menor
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5)//Gera números aleatório 
+//console.log(Math.pow(2, 5))
+
+//Math.pow Porencia(2,5)
