79c76fafe9ab924b68074ddaa30049204e116637
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-10-29 15:31:11 -0300

    deletando array

--- JS/aula16(arrays)/index.js
+++ JS/aula16(arrays)/index.js
@@ -1,11 +1,22 @@
-// function soma(x = 1, y = 1) {
-// const resultado = x + y;
-// return resultado;
-// }
+//                0           1       2
+const alunos = ['Gabryel', 'Luiz', 'Jorge'];
 
-// const resultado = soma(9, 9)
-// console.log(resultado )
-const raiz = n =>  n ** 0.5
+delete alunos[1]
+
+//alunos.push('Gomez');//adiciona nome no fim
+//alunos.unshift('Luiza')//Adicionar elementos no começo do array
+
+//alunos.pop()//Remove o último elemento do array
+
+//alunos.shift()//Remove o primeiro elemento do array
 
 
-console.log(raiz(9))
+console.log(alunos);
+//alunos[0] = 'João';
+//alunos[3] = 'Marina';
+
+/*
+console.log(alunos[0]);
+console.log(alunos[1]);
+console.log(alunos[2])
+*/
