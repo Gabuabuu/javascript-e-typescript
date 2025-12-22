27240b6e6eed3937bc4911214f83a23c0d2061b1
Gabuabuu <gabydavilaneves@gmail.com>
2025-12-22 13:10:42 -0300

    feat:

--- JS/aula26(IMC)/assets/js/main.js
+++ JS/aula26(IMC)/assets/js/main.js
@@ -3,16 +3,41 @@ const form = document.getElementById("form-imc");
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  showResult();
+
+  const inputPeso = e.target.querySelector('#peso');
+  const inputAltura = e.target.querySelector('#altura');
+
+  const peso = Number(inputPeso.value)
+  const altura = Number(inputAltura.value)
+
+  if (!peso) {
+    showResult('Preencha o campo "peso" corretamente', false)
+    return;
+  }
+  if (!altura) {
+    showResult('Preencha o campo "altura" corretamente', false)
+    return;
+  }
+  const imc = getImc(peso, altura);
+  console.log('teste')
 })
 
-const showResult = (msg) => {
+function getImc () {
+  const imc = peso / altura ** 2
+}
+
+
+function criaParagrafo() {
+  const p = document.createElement('p');
+  return p;
+}
+
+const showResult = (msg, isValid) => {
   const resultado = document.getElementById("resultado");
   resultado.innerHTML = ``;
 
-  const p = document.createElement('p');
-   p.classList.add('paragrafo-result')
-  p.innerHTML = ('Paragrafo');
-  resultado.appendChild(p);
+  const paragrafo = criaParagrafo()
+  paragrafo.innerHTML = msg
+  resultado.appendChild(paragrafo)
 }
 
