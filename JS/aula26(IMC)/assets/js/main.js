0637ff406e8daf1d4301928b0a707c4435e2843a
Gabuabuu <gabydavilaneves@gmail.com>
2026-03-05 10:32:13 -0300

    feat: :sparkles: Add setColor
    Adding and creating "setColor" function to set BG color to imcList depends on imc index

--- JS/aula26(IMC)/assets/js/main.js
+++ JS/aula26(IMC)/assets/js/main.js
@@ -40,43 +40,59 @@ Obesidade grau 3
 
  */
 
-
+function setColor(color) {
+  paragrafo.style.backgroundColor = color
+}
 
 function getImcList(imc) {
-  const color = paragrafo
   const imcList = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
     'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
   if (imc >= 39.9) {
-    color.style.backgroundColor = 'purple'
+    setColor('#D40D0D')
     return imcList[5]
-  } 
-}
-
-function getImc(peso, altura) {
-  const imc = peso / altura ** 2
-  return imc.toFixed(2)
-}
-
-
-function criaParagrafo() {
-  const p = document.createElement('p');
-  return p;
-}
-const paragrafo = criaParagrafo()
-
-const showResult = (msg, isValid) => {
-  const resultado = document.getElementById("resultado");
-  resultado.innerHTML = `test`;
-
-
-
-  if (isValid) {
-    paragrafo.classList.add('paragrafo-resultado')
+  } else if (imc >= 35.9 && imc <= 39.9) {
+    setColor('#b85702ff')
+    return imcList[4]
+  } else if (imc >= 30 && imc <= 34.9) {
+    setColor('#D4CA0D')
+    return imcList[3]
+  } else if (imc >= 25 && imc <= 29.9) {
+    setColor('#6bc21aff')
+    return imcList[2]
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    setColor('#1df106ff')
+    return imcList[1]
   } else {
-    paragrafo.classList.add('bad')
+    imc < 18
+    return imcList[0]
+  }
+}
+
+  function getImc(peso, altura) {
+    const imc = peso / altura ** 2
+    return imc.toFixed(2)
   }
 
-  paragrafo.innerHTML = msg
-  resultado.appendChild(paragrafo)
-}
+
+  const paragrafo = criaParagrafo()
+  function criaParagrafo() {
+    const p = document.createElement('p');
+    return p;
+  }
+
+  const showResult = (msg, isValid) => {
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = ``;
+
+
+
+    if (isValid) {
+      setColor('green')
+    } else {
+      setColor('red')
+    }
+
+    paragrafo.innerHTML = msg
+    resultado.appendChild(paragrafo)
+  }
 
