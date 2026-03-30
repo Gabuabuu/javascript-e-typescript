b6009cd065064cfb644e02c41c2d3d29840b3a58
Gabuabu <gabydavilaneves@gmail.com>
2026-03-30 08:51:12 -0300

    feat: Aula sobre os tipos de For

--- JS/aula36(For of - Estrutura de repetição)/index.js
+++ JS/aula36(For of - Estrutura de repetição)/index.js
@@ -1,5 +1,18 @@
-const nome = 'Aaaaa'
+const nome = ['Gabryel Neves', 'Pedro', 'Joj']
 
-for (let i =0; i < nome.length; i++){
-  console.log(nome[i])
+// for (let i =0; i < nome.length; i++){
+//   console.log(nome[i])
+// }
+
+// for (let i in nome ) {
+//   console.log(nome[i])
+// }
+
+for (let valor of nome) {
+  console.log(valor)
 }
+
+
+nome.forEach(function(e, i) {
+  console.log(e, i)
+})
